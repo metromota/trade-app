851ba84f7d5577c2934bbee53a1c3fa84fa70ada
hertonmota <herton.mota@digifred.com.br>
2023-05-20 10:05:58 -0300

    add: public routes

---
+++ src/app/routing/public/index.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from '@pages/public/login/login.component';
+
+const Routes: Routes = [
+    {
+        path: '', 
+        component: LoginComponent,
+    }
+   
+]
+
+export { Routes }
