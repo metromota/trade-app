8644affcfec4894985e02ab55f938e42968de447
hertonmota <herton.mota@digifred.com.br>
2023-05-20 10:09:13 -0300

    add: error-input component

---
+++ src/app/share/components/error-input/error-input.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ErrorInputComponent } from './error-input.component';
+
+describe('ErrorInputComponent', () => {
+  let component: ErrorInputComponent;
+  let fixture: ComponentFixture<ErrorInputComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ErrorInputComponent]
+    });
+    fixture = TestBed.createComponent(ErrorInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
