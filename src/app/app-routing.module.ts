1aa69e9b12e682809216871950fe07e9246699a9
hertonmota <herton.mota@digifred.com.br>
2023-05-20 16:49:43 -0300

    add: guard of private route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthService } from '@core/services/auth.service';
 import { LoginComponent } from '@pages/public/login/login.component';
 
 const routes: Routes = [
@@ -10,6 +11,7 @@ const routes: Routes = [
   },
   {
     path: 'dashboard',
+    canActivate: [AuthService],
     loadChildren: () => import('@pages/private/dashboard/dashboard.module')
       .then(m => m.DashboardModule)
   }
