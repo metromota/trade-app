ae86e6f38c05b2bada0b0c8bc9ff7f80dc55a27c
hertonmota <herton.mota@digifred.com.br>
2023-05-20 10:08:06 -0300

    add: form login

---
+++ src/app/pages/public/login/form/form.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Login } from '../model/login.model';
+
+@Component({
+  selector: 'trade-login-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent {
+
+  formLogin: FormGroup;
+  apikey: FormControl;
+
+  constructor(private formbuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.createControls();
+    this.createForm();
+  }
+
+  createControls() {
+    const apikeyValidators = [
+      Validators.required,
+      Validators.minLength(3)
+    ]
+    this.apikey = new FormControl('', Validators.compose(apikeyValidators))
+  }
+
+  createForm() {
+    this.formLogin = this.formbuilder.group({
+      apikey: this.apikey
+    })
+  }
+
+  submitLogin() {
+    console.log(this.formLogin.value)
+  }
+
+}
