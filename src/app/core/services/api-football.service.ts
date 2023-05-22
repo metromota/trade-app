82bd8d077250e71158530e824f1d74e5c5ca77b3
hertonmota <herton.mota@digifred.com.br>
2023-05-21 22:46:06 -0300

    refactor: api service

--- src/app/core/services/api-football.service.ts
+++ src/app/core/services/api-football.service.ts
@@ -1,17 +1,19 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Environment } from "environments/enviroments";
+import { Environment } from "@core/environments/enviroments";
 import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class ApiFootbalService {
+
     urlApi: string = Environment.getApiUrl();
+
     constructor(private http: HttpClient) {
     }
 
-    authentication(): Observable<any> {
+    authentication(apikey: string): Observable<any> {
         return this.http.get(`${this.urlApi}/authentication`);
     }
 
@@ -19,8 +21,24 @@ export class ApiFootbalService {
         return this.http.get(`${this.urlApi}/countries`);
     }
 
-    getTemporaries(): Observable<any> {
-        return this.http.get(`${this.urlApi}/temporaries`);
+    getLeague(id: number): Observable<any> {
+        return this.http.get(`${this.urlApi}/leagues${id}`);
+    }
+
+    getLeaguesForCountry(country: string): Observable<any> {
+        return this.http.get(`${this.urlApi}/leagues?country=${country}`);
+    }
+
+    getSeasons(): Observable<any> {
+        return this.http.get(`${this.urlApi}/leagues/seasons`);
+    }
+
+    getTeamsOfSeason(season: number, leagueid: number): Observable<any> {
+        return this.http.get(`${this.urlApi}/teams?league=${leagueid}&season=${season}`);
+    }
+
+    getTeam(id: number): Observable<any> {
+        return this.http.get(`${this.urlApi}/teams/${id}`);
     }
 
 }
