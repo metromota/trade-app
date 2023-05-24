5025b3f53700b97c2ea62ce759219e7929be9136
hertonmota <herton.mota@digifred.com.br>
2023-05-23 22:52:26 -0300

    refactor: login component

--- src/app/pages/public/login/form/form.component.ts
+++ src/app/pages/public/login/form/form.component.ts
@@ -1,44 +1,42 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    Validators,
+} from '@angular/forms';
 
 @Component({
-  selector: 'trade-login-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+    selector: 'trade-login-form',
+    templateUrl: './form.component.html',
+    styleUrls: ['./form.component.scss'],
 })
 export class FormComponent {
+    formLogin: FormGroup;
+    apikey: FormControl;
 
-  formLogin: FormGroup;
-  apikey: FormControl;
+    @Output() login = new EventEmitter<string>();
 
-  @Output() login = new EventEmitter<string>();
+    constructor(private formbuilder: FormBuilder) {}
 
-  constructor(
-    private formbuilder: FormBuilder,
-  ) { }
+    ngOnInit() {
+        this.createControls();
+        this.createForm();
+    }
 
-  ngOnInit() {
-    this.createControls();
-    this.createForm();
-  }
+    createControls() {
+        const apikeyValidators = [Validators.required, Validators.minLength(3)];
+        this.apikey = new FormControl('', Validators.compose(apikeyValidators));
+    }
 
-  createControls() {
-    const apikeyValidators = [
-      Validators.required,
-      Validators.minLength(3)
-    ]
-    this.apikey = new FormControl('', Validators.compose(apikeyValidators))
-  }
+    createForm() {
+        this.formLogin = this.formbuilder.group({
+            apikey: this.apikey,
+        });
+    }
 
-  createForm() {
-    this.formLogin = this.formbuilder.group({
-      apikey: this.apikey
-    })
-  }
-
-  submitLogin() {
-    const login = this.formLogin.value
-    this.login.emit(login)
-  }
-
+    submitLogin() {
+        const login = this.formLogin.value;
+        this.login.emit(login);
+    }
 }
--- src/app/pages/public/login/login.component.ts
+++ src/app/pages/public/login/login.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
+import { Security } from '@core/security/security';
+import { LocalStorageService } from '@core/services/localstorage.service';
+import { ApiFootbalService } from '@core/services/api-football.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  authenticate(event){
-    console.log(event)
-  }
+    constructor(
+        private api: ApiFootbalService,
+        private router: Router,
+        private localstorage: LocalStorageService
+    ) {}
+
+    authenticate(event) {
+        const { apikey } = event;
+        Security.setApikey(apikey);
+
+        const nextObservable = (resp) => {
+            const { response } = resp;
+            const responseInvalid = Array.isArray(response);
+
+            if (responseInvalid) {
+                alert('Apikey inválido');
+                return;
+            }
+
+            const isActive = this.isActiveAccount(response);
+
+            if (isActive) {
+                this.localstorage.set('active', true);
+                this.router.navigate(['/dashboard']);
+            } else {
+                Security.removeApikey();
+                this.localstorage.clear();
+            }
+        };
+
+        const error = (err) => {
+            Security.removeApikey();
+            this.localstorage.clear();
+        };
+
+        this.api.authentication().subscribe(nextObservable, error);
+    }
+
+    isActiveAccount({ subscription }) {
+        return !!subscription.active;
+    }
 }
